863a48a5ddb00a8fe545e98058f9a7fe9d2eafe6
deglaus <johan.visteby@gmail.com>
2023-04-12 09:44:35 +0200

    this gapbs

--- profiling_bfs.sh
+++ profiling_bfs.sh
@@ -5,19 +5,25 @@
 
 echo "Running BFS on Kronecker graph"
 
-sudo perf record -e L1-dcache-load-misses:P -c 100 -g -- sleep 5 ./bfs -g 20 -n 1
+#sudo perf record -e L1-dcache-load-misses -c 100 -g -- sleep 5 ./bfs -g 20 -n 1
 
-"Kronecker" >> bfs.txt
-"----------------------" >> bfs.txt
+#sudo perf record -e L1-dcache-load-misses ./bfs -g 20 -n 1
+# ./converter -g 20 -b krongraph.sg
+# ./converter -u 20 -b uniformgraph.el
+sudo perf record -e L1-dcache-load-misses ./bfs -f krongraph.sg
+
+
+echo "Kronecker" >> bfs.txt
+echo "----------------------" >> bfs.txt
 sudo perf annotate --stdio >> bfs.txt
 
-echo "Running BFS on uniform graph"
+# echo "Running BFS on uniform graph"
 
-sudo perf record -e L1-dcache-load-misses:P -c 100 -g -- sleep 5 ./bfs -u 20 -n 1
+# sudo perf record -e L1-dcache-load-misses -c 100 -g -- sleep 5 ./bfs -u 20 -n 1
 
-"Uniform" >> bfs.txt
-"----------------------" >> bfs.txt
-sudo perf annotate --stdio >> bfs.txt
+# echo "Uniform" >> bfs.txt
+# echo "----------------------" >> bfs.txt
+# sudo perf annotate --stdio >> bfs.txt
 
 
 
