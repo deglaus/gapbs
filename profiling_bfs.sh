0e5b30bffe0b8ed5b27576e45840de920eca30c0
deglaus <johan.visteby@gmail.com>
2023-04-12 08:58:35 +0200

    execute and store data

--- profiling_bfs.sh
+++ profiling_bfs.sh
@@ -1,11 +1,23 @@
 # Runs the first rounds of tests
 
+# :P -- precision does not work on laptop
+# Works on stationary
 
-echo "Running BFS"
+echo "Running BFS on Kronecker graph"
 
 sudo perf record -e L1-dcache-load-misses:P -c 100 -g -- sleep 5 ./bfs -g 20 -n 1
 
-# sudo perf annotate --stdio
-sudo perf script --header
+"Kronecker" >> bfs.txt
+"----------------------" >> bfs.txt
+sudo perf annotate --stdio >> bfs.txt
+
+echo "Running BFS on uniform graph"
+
+sudo perf record -e L1-dcache-load-misses:P -c 100 -g -- sleep 5 ./bfs -u 20 -n 1
+
+"Uniform" >> bfs.txt
+"----------------------" >> bfs.txt
+sudo perf annotate --stdio >> bfs.txt
+
 
 
