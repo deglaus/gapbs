5a9f3352e7a267f66fb5bdf5bdaa4ab68abcd108
deglaus <johan.visteby@gmail.com>
2023-04-14 16:38:15 +0200

    figuring it out

--- profiling_bfs.sh
+++ profiling_bfs.sh
@@ -10,22 +10,39 @@ echo "Running BFS on Kronecker graph"
 #sudo perf record -e L1-dcache-load-misses ./bfs -g 20 -n 1
 
 # Record with high-level instructions; no stack-trace.
- sudo perf record -e L1-dcache-load-misses:P ./bfs -f ./benchmark/krongraph.sg
-# easy to read with perfo annotate!
+# sudo perf record -e L1-dcache-load-misses:P ./bfs -f ./benchmark/krongraph.sg
+# easy to read with perf annotate!
 
 
 # Record with instructions with stack-trace.
-# sudo perf record -e L1-dcache-load-misses:P -c 100 -g -- sleep 5 ./bfs -f ./benchmark/krongraph.sg -n 3
+
+## Using sleep
+#  sudo perf record -e L1-dcache-load-misses:P -c 100 -g -- sleep 5 ./bfs -f ./benchmark/krongraph.sg -n 3
+
+# With dwarf -- seems like the most suitable one... it's easy to ready with: sudo perf report --call-graph --stdio
+# or just sudo perf report
+ sudo perf record -g  -e L1-dcache-load-misses:P --call-graph dwarf ./bfs -f ./benchmark/krongraph.sg
+
+
+# With dwarf + sleep -- seems to exclusively produce stack traces to the Linux kernel.
+#  sudo perf record -g  -e L1-dcache-load-misses:P -c 100 --call-graph dwarf sleep 5  ./bfs -f ./benchmark/krongraph.sg -n 3
 
 
 
-echo "Kronecker" >> bfs.txt
-echo "----------------------" >> bfs.txt
-sudo perf annotate --stdio >> bfs.txt
 
-# echo "Running BFS on uniform graph"
+# Present results
+# sudo perf report --call-graph --stdio
+# sudo perf annotate -l --full-paths --source   --stdio
+
+# echo "Kronecker" >> bfs.txt
+# echo "----------------------" >> bfs.txt
+# sudo perf annotate --stdio >> bfs.txt
+
+ echo "Running BFS on uniform graph"
 
 # sudo perf record -e L1-dcache-load-misses -c 100 -g -- sleep 5 ./bfs -u 20 -n 1
+ sudo perf record -g  -e L1-dcache-load-misses:P --call-graph dwarf ./bfs -f ./benchmark/uniformgraph.sg
+
 
 # echo "Uniform" >> bfs.txt
 # echo "----------------------" >> bfs.txt
